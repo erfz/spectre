b25aa0338c8676404f10460d14526bd3fec4eaa0
Jason Guo <jsg353@cornell.edu>
2020-07-22 17:37:53 -0400

    Renaming of class

--- src/ParallelAlgorithms/Events/ObserveStrahlkorper.hpp
+++ src/ParallelAlgorithms/Events/ObserveStrahlkorper.hpp
@@ -39,19 +39,19 @@ namespace dg {
 namespace Events {
 template <typename ObservationValueTag, typename Tensors,
           typename EventRegistrars>
-class ObserveErrorNorms;
+class ObserveStrahlkorper;
 
 namespace Registrars {
 template <typename ObservationValueTag, typename Tensors>
-using ObserveErrorNorms =
-    ::Registration::Registrar<Events::ObserveErrorNorms, ObservationValueTag,
+using ObserveStrahlkorper =
+    ::Registration::Registrar<Events::ObserveStrahlkorper, ObservationValueTag,
                               Tensors>;
 }  // namespace Registrars
 
 template <typename ObservationValueTag, typename Tensors,
           typename EventRegistrars = tmpl::list<
-              Registrars::ObserveErrorNorms<ObservationValueTag, Tensors>>>
-class ObserveErrorNorms;  // IWYU pragma: keep
+              Registrars::ObserveStrahlkorper<ObservationValueTag, Tensors>>>
+class ObserveStrahlkorper;  // IWYU pragma: keep
 
 /*!
  * \ingroup DiscontinuousGalerkinGroup
@@ -72,7 +72,7 @@ class ObserveErrorNorms;  // IWYU pragma: keep
  */
 template <typename ObservationValueTag, typename... Tensors,
           typename EventRegistrars>
-class ObserveErrorNorms<ObservationValueTag, tmpl::list<Tensors...>,
+class ObserveStrahlkorper<ObservationValueTag, tmpl::list<Tensors...>,
                         EventRegistrars> : public Event<EventRegistrars> {
  private:
   template <typename Tag>
@@ -92,9 +92,9 @@ class ObserveErrorNorms<ObservationValueTag, tmpl::list<Tensors...>,
 
  public:
   /// \cond
-  explicit ObserveErrorNorms(CkMigrateMessage* /*unused*/) noexcept {}
+  explicit ObserveStrahlkorper(CkMigrateMessage* /*unused*/) noexcept {}
   using PUP::able::register_constructor;
-  WRAPPED_PUPable_decl_template(ObserveErrorNorms);  // NOLINT
+  WRAPPED_PUPable_decl_template(ObserveStrahlkorper);  // NOLINT
   /// \endcond
 
   using options = tmpl::list<>;
@@ -111,7 +111,7 @@ class ObserveErrorNorms<ObservationValueTag, tmpl::list<Tensors...>,
       "triggered at a given observation value.  Causing multiple events to\n"
       "run at once will produce unpredictable results.";
 
-  ObserveErrorNorms() = default;
+  ObserveStrahlkorper() = default;
 
   using observed_reduction_data_tags =
       observers::make_reduction_data_tags<tmpl::list<ReductionData>>;
@@ -169,7 +169,7 @@ class ObserveErrorNorms<ObservationValueTag, tmpl::list<Tensors...>,
 /// \cond
 template <typename ObservationValueTag, typename... Tensors,
           typename EventRegistrars>
-PUP::able::PUP_ID ObserveErrorNorms<ObservationValueTag, tmpl::list<Tensors...>,
+PUP::able::PUP_ID ObserveStrahlkorper<ObservationValueTag, tmpl::list<Tensors...>,
                                     EventRegistrars>::my_PUP_ID = 0;  // NOLINT
 /// \endcond
 }  // namespace Events
